081b29e6282b4d13110ed44912a47bf241bc839d
Kristine Laing <kristal.tasha@gmail.com>
2022-06-16 23:29:37 +0000

    navbar flex done

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
-import { Navbar, NavbarBrand } from 'reactstrap'
+import React from 'react';
+import './styles/navbar.scss'
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap'
 
 export default function Navigation() {
   return (
     <Navbar color="secondary" expand="md" light>
-      <NavbarBrand href='#' font-family='Italiana' >PHIX</NavbarBrand>
+      <NavbarBrand className='navbar-brand' href='#' font-family='Italiana' >PHIX</NavbarBrand>
+      <Nav navbar>
+          <NavItem>
+            <NavLink href='#'>About</NavLink>
+          </NavItem>
+
+          <NavItem>
+            <NavLink href='#'>Contact</NavLink>
+          </NavItem>
+
+          <NavItem>
+            <NavLink href='#'>Services</NavLink>
+          </NavItem>
+      </Nav>
+
+      <Nav navbar>
+          <NavItem>
+            <NavLink className='login-link' href='#'>Login</NavLink>
+          </NavItem>
+
+          <NavItem>
+            <NavLink href='#'>Signup</NavLink>
+          </NavItem>
+      </Nav>
+
 
     </Navbar>
   )
